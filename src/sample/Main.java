4edf89abf3b332e23fd2b282b54ce21a9c294b62
Maxg2212 <maxgarro22@gmail.com>
2020-09-21 00:25:43 -0600

    [Update] Parte final del proyecto

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,20 +11,40 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
-
+//Se debe permitir el parallel run del main para que funcione.
 
 public class Main extends Application {
 
+    private boolean isServer = false; //True para abrir Server y false para abrir Client
+
     private TextArea messages = new TextArea();
+    private NetworkConnection connection = isServer ? createServer() : createClient();
 
     private Parent createContent() {
         messages.setPrefHeight(550);
         TextField input = new TextField();
+        input.setOnAction(event -> {
+            String message = isServer ? "Server: " : "Client: ";
+            message += input.getText();
+            input.clear();
 
+            messages.appendText(message + "\n");
+
+            try {
+                connection.send(message);
+            }
+            catch (Exception e) {
+                messages.appendText("Failed to send\n");
+            }
+        });
 
         VBox root = new VBox(20, messages, input);
         root.setPrefSize(600,600);
         return root;
+    }
+    @Override
+    public void init() throws Exception {
+        connection.startConnection();
     }
 
     @Override
@@ -32,6 +53,26 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(createContent()));
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        connection.closeConnection();
+    }
+    private Server createServer() {
+        return new Server(55555, data -> {
+            Platform.runLater(() -> {
+               messages.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+    private Client createClient() {
+        return new Client("127.0.0.1", 55555, data -> {
+            Platform.runLater(() -> {
+                messages.appendText(data.toString() + "\n");
+            });
+        });
     }
 
 
